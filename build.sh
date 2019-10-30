ef14219b0da6a2bb0301746c1070a2d68f71605e
cursinglizard <unrealguthrie@gmail.com>
2019-10-30 23:56:44 +0100

    Updated build-shellscript

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 echo "Start building project."
 
+java -jar ./XSDL/HeaderJoiner.jar -o ./XSDL/xsdl.h -t ./XSDL/headers/XSDL.h ./XSDL/headers/XSDL_node.h ./XSDL/headers/XSDL_context.h ./XSDL/headers/XSDL_render.h ./XSDL/headers/XSDL_text.h
+
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 OBJECTDIR="${DIR}/objects"
 BUILDDIR="${DIR}/build"

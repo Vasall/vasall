5dad46878a68fd6a2d8d57664efe31d022c3c2f1
Julian Kennerknecht <admin@vasall.net>
2021-04-06 00:07:58 +0200

    Update amoloader.h

--- lib/amoloader/amoloader.h
+++ lib/amoloader/amoloader.h
@@ -1,7 +1,7 @@
 /*
  * zlib License
  * 
- * (C) 2020 clusterwerk
+ * (C) 2021 clusterwerk
  * 
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
